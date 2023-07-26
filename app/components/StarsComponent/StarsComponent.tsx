1e88279d77fc1e82f41606fa02df91f3aae2e498
B45T13N <87314220+B45T13N@users.noreply.github.com>
2023-07-26 11:55:52 +0200

    Feature/totale notation (#3)
    * Add TotalNotationComponent.tsx
    
    * Setup the conditionnal display
    
    * Setup TotalNotationComponent.tsx
    
    * Clean code due to warnings

--- app/components/StarsComponent/StarsComponent.tsx
+++ app/components/StarsComponent/StarsComponent.tsx
@@ -6,13 +6,16 @@ import { useState, useEffect } from 'react';
 interface StarsComponentProps
 {
     title: string;
+    index: number;
+    notation: number;
+    setNotations: (index:number, notation:number) => void | null ;
 }
 
-function StarsComponent({title}: StarsComponentProps)
+function StarsComponent(props: StarsComponentProps)
 {    
     const totalStars: number = 5;
     const [rating, setRating] = useState<number | null>(null);
-    const [indexCliqued, setIndexCliqued] = useState<number | null>(null);
+    const [indexClicked, setIndexClicked] = useState<number | null>(null);
     const [starsData, setStarsData] = useState<boolean[]>(Array(totalStars).fill(false));
     
     const handleStarClick = (indexStarClicked: number) => {
@@ -27,13 +30,14 @@ function StarsComponent({title}: StarsComponentProps)
     }
 
     useEffect(() => {
-        if(rating != null && rating >= 1){            
-            setIndexCliqued(rating);
+        if(rating != null && rating >= 1){
+            setIndexClicked(rating);
+            props.setNotations(props.index, rating);
         }
         else{
-            setIndexCliqued(0);
+            setIndexClicked(0);
         }
-    }, [rating])
+    }, [props, rating])
 
 
     const stars = starsData.map((filled, index) => (
@@ -43,14 +47,14 @@ function StarsComponent({title}: StarsComponentProps)
         onClick={handleStarClick} 
         filled={filled} 
         onMouseEnter={handleStarsHover}
-        onMouseLeave={() => setStarsData(starsData.map((_, i) => i < (indexCliqued || 0)))}
-        onStarBounce={indexCliqued != null && index < indexCliqued}
+        onMouseLeave={() => setStarsData(starsData.map((_, i) => i < (indexClicked || 0)))}
+        onStarBounce={indexClicked != null && index < indexClicked}
         />
     ))
 
     return(
         <div>
-            <h2 className="text-center text-2xl">{title}</h2>
+            <h2 className="text-center text-2xl">{props.title}</h2>
             <div className="">
                 {stars}
             </div>
---
+++ app/components/TotaleNotationComponent/TotalNotationComponent.tsx
@@ -0,0 +1,83 @@
+import "./TotalNotationComponent.css"
+import React, {ReactElement, useState} from "react";
+
+interface TotalNotationComponentProps
+{
+    average: number
+}
+
+function TotalNotationComponent(props: TotalNotationComponentProps)
+{
+    const totalStars: number = 5;
+    const stopColor = "#f8d300";
+    const [starsData] = useState<boolean[]>(Array(totalStars).fill(false));
+
+    const createStars  = (average: number) => {
+
+        let stars: ReactElement[]= starsData.map((filled, index) =>
+            {
+                let offsetYellow: number = 0;
+                let offsetWhite: number = 0;
+                if(average - index < 0){
+                    offsetYellow = (average - index) * 100
+                    offsetWhite = 0
+                }
+                else if (average - index > 0) {
+                    offsetYellow = (average - index) * 100
+                    offsetWhite = 100 - offsetYellow
+                }
+
+                if(offsetYellow < 0){
+                    offsetYellow = 0
+                    offsetWhite = 100
+                }
+
+
+                return (
+
+                    <span key={index} className={'total'}>
+                        <svg width="50px" height="50px" viewBox="120 120 80 80">
+                            <linearGradient id={`fill${index}`} x1="0%" y1="0%" x2="100%" y2="0%">
+                                {offsetYellow != 0 ? <stop offset={`${offsetYellow}%`} stopColor={stopColor} /> : ''}
+                                {offsetWhite != 0 ? <stop offset={`${offsetWhite}%`} stopColor="#fff" /> : ''}
+                            </linearGradient>
+
+                            <path d="M 160.000 180.000
+                                    L 183.511 192.361
+                                    L 179.021 166.180
+                                    L 198.042 147.639
+                                    L 171.756 143.820
+                                    L 160.000 120.000
+                                    L 148.244 143.820
+                                    L 121.958 147.639
+                                    L 140.979 166.180
+                                    L 136.489 192.361
+                                    L 160.000 180.000"
+                                  fill={`url(#fill${index})`}/>
+                      </svg>
+                    </span>
+                )
+            }
+        )
+
+        return stars;
+    }
+
+    const stars: Array<ReactElement> = createStars(props.average)
+
+    return (
+        <div>
+            <div>
+                {props.average}
+                {stars}
+            </div>
+            <div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TotalNotationComponent
